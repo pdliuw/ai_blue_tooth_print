4c030f10550dce75d8bdd6c5cc673211bc99535b
Air <xkrossiapd163@163.com>
2020-01-31 15:08:27 +0800

    Optimize: project structure

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.2]
+
+* Optimize: project structure
+
 ## [0.1.1]
 
 * Update: usage document
--- lib/ai_blue_tooth_print.dart
+++ lib/ai_blue_tooth_print.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
-import 'package:flutter/services.dart';
 
 ///
 /// AiBlueToothPrint
